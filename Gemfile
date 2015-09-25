42f8216bb51123b4948fe1c264c0ac7130d38464
Sean Luckett <sluckett@itriagehealth.com>
2015-09-25 13:59:19 -0600

    Ready to roll.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'rspec', '~>3.3.0'
+gem 'pry', '~>0.10.1'
---
+++ spec/string_calc_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+describe 'String Calculator' do
+
+  describe '#add' do
+    it 'start here' do
+    end
+  end
+
+end
